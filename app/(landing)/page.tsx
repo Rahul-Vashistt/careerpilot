27b26fadb880db5f98675f6d7032f4a2423f60e6
Rahul Vashist <rahulvashist14102004@gmail.com>
2026-09-09 18:59:04 +0530

    Initial commit

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function landing() {
+  return (
+    <div className="min-h-screen text-white text-9xl flex items-center justify-center capitalize tracking-wider">
+      hello
+    </div>
+  );
+}
